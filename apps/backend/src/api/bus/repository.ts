dc019409d3f997e5972963234fc94c1ebac32b75
mohamedadel46 <muhammed195826@feng.bu.edu.eg>
2024-05-02 18:40:49 +0300

    added repo mongodb

--- apps/backend/src/api/bus/repository.ts
+++ apps/backend/src/api/bus/repository.ts
@@ -1,86 +1,35 @@
-import { Bus, CreateBusDto } from '@/api/bus/model';
+import { CreateReservationDto, Reservation, ReservationModel } from './model';
 
-export const buses: Bus[] = [
-  { id: 1, name: 'Bus 1 Matrouh-Delta', areaNumber: 1, createdAt: new Date(), updatedAt: new Date() },
 
-  { id: 2, name: 'Bus 2 Matrouh-Delta', areaNumber: 2, createdAt: new Date(), updatedAt: new Date() },
 
-  { id: 3, name: 'Bus 3 Matrouh-Delta', areaNumber: 3, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 4, name: 'Bus 4 Matrouh-Delta', areaNumber: 4, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 5, name: 'Bus 5 Matrouh-Delta', areaNumber: 5, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 6, name: 'Bus 6 Matrouh-Delta', areaNumber: 6, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 7, name: 'Bus 7 Matrouh-Delta', areaNumber: 7, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 8, name: 'Bus 8 Matrouh-Delta', areaNumber: 8, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 9, name: 'Bus 9 Matrouh-Delta', areaNumber: 9, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 10, name: 'Bus 10 Matrouh-Delta', areaNumber: 10, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 11, name: 'Bus 11 Matrouh-Delta', areaNumber: 11, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 12, name: 'Bus 12 Matrouh-Delta', areaNumber: 12, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 13, name: 'Bus 13 Matrouh-Delta', areaNumber: 13, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 14, name: 'Bus 14 Matrouh-Delta', areaNumber: 14, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 15, name: 'Bus 15 Matrouh-Delta', areaNumber: 15, createdAt: new Date(), updatedAt: new Date() },
-
-  { id: 16, name: 'Bus 16 Matrouh-Delta', areaNumber: 16, createdAt: new Date(), updatedAt: new Date() },
-];
-
-export const busRepository = {
-  findAllAsync: async (): Promise<Bus[]> => {
-    return buses;
+export const reservationRepository = {
+  findAllAsync: async (): Promise<Reservation[]> => {
+    return ReservationModel.find().exec();
   },
 
-  findByIdAsync: async (id: number): Promise<Bus | undefined> => {
-    const resBuses = buses.filter((bus) => bus.id === id);
-    if (resBuses.length === 0) {
-      return;
+  findByIdAsync: async (id: number): Promise<Reservation | null> => {
+    return ReservationModel.findById(id).exec();
+  },
+
+  findByOwnerIdAsync: async (id: number): Promise<Reservation | null> => {
+    return ReservationModel.findOne({ id_owner: id }, (err:string, result:Promise<Reservation | null>) => {
+    if (err) {
+        console.error(err);
+        return;
     }
-    return resBuses[0];
+    console.log(result);
+}).exec();
   },
 
-  createAsync: async (bus: CreateBusDto): Promise<Bus> => {
-    const newBus = {
-      ...bus,
-      id: buses.length + 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-
-    buses.push(newBus);
-    return newBus;
-  },
-  deleteAsync: async (id: number): Promise<Bus | undefined> => {
-    const resBuses = buses.filter((bus) => bus.id === id);
-    if (resBuses.length === 0) {
-      return;
-    }
-    const index = buses.indexOf(resBuses[0]);
-    buses.splice(index, 1);
-    return resBuses[0];
+  update: async (id: number, data: Reservation) => {
+   return ReservationModel.findByIdAndUpdate(id, data, { new: true }).exec();
   },
 
-  patchAsync: async (id: number, bus: Partial<Bus>): Promise<Bus | undefined> => {
-    const resBuses = buses.filter((bus) => bus.id === id);
+  push: async (data: CreateReservationDto): Promise<Reservation | null> => {
+   return ReservationModel.create(data);
+  },
 
-    if (resBuses.length === 0) {
-      return;
-    }
-
-    const index = buses.indexOf(resBuses[0]);
-
-    buses[index].name = bus.name ?? buses[index].name;
-    buses[index].areaNumber = bus.areaNumber ?? buses[index].areaNumber;
-    buses[index].updatedAt = new Date();
-
-    return buses[index];
+  delete: async (id: number): Promise<Reservation | null> => {
+   return  ReservationModel.findByIdAndDelete(id).exec();
   },
 };
