37ad9916b421a4b08068087720e9bfc4030bce58
mohamedadel46 <muhammed195826@feng.bu.edu.eg>
2024-05-02 18:48:50 +0300

    added repo mongodb

--- apps/backend/src/api/reservations/repository.ts
+++ apps/backend/src/api/reservations/repository.ts
@@ -1,46 +1,35 @@
-import { Reservation } from '@/api/reservations/model';
+import { CreateReservationDto, Reservation, ReservationModel } from './model';
 
-export const reservations: Reservation[] = [
-  { id: 1, ticket_number: 1, price: 50, id_owner: 2, id_trip: 42, id_bus: 5, createdAt: new Date() },
-  { id: 2, ticket_number: 2, price: 20, id_owner: 5, id_trip: 40, id_bus: 3, createdAt: new Date() },
-];
+
 
 export const reservationRepository = {
   findAllAsync: async (): Promise<Reservation[]> => {
-    return reservations;
+    return ReservationModel.find().exec();
   },
 
   findByIdAsync: async (id: number): Promise<Reservation | null> => {
-    return reservations.find((reservations) => reservations.id === id) || null;
+    return ReservationModel.findById(id).exec();
   },
 
   findByOwnerIdAsync: async (id: number): Promise<Reservation | null> => {
-    return reservations.find((reservations) => reservations.id_owner === id) || null;
-  },
-
-  update: async (id: number, data: Reservation): Promise<Reservation | undefined> => {
-    const index = reservations.findIndex((reservation) => reservation.id === id);
-    if (index !== -1) {
-      reservations[index] = { ...reservations[index], ...data, id: id };
-      return reservations[index];
+    return ReservationModel.findOne({ id_owner: id }, (err:string, result:Promise<Reservation | null>) => {
+    if (err) {
+        console.error(err);
+        return;
     }
-    return undefined;
+    console.log(result);
+}).exec();
   },
 
-  push: async (data: Reservation): Promise<Reservation | null> => {
-    const newData = { ...data, id: reservations.length + 1 };
-    console.log(newData);
-    reservations.push(newData);
-    return newData;
+  update: async (id: number, data: Reservation) => {
+   return ReservationModel.findByIdAndUpdate(id, data, { new: true }).exec();
+  },
+
+  push: async (data: CreateReservationDto): Promise<Reservation | null> => {
+   return ReservationModel.create(data);
   },
 
   delete: async (id: number): Promise<Reservation | null> => {
-    const index = reservations.findIndex((reservation) => reservation.id === id);
-    if (index !== -1) {
-      const deletedReservation = reservations.splice(index, 1)[0];
-      return deletedReservation ? deletedReservation : null;
-    }
-    console.log(reservations);
-    return null;
+   return  ReservationModel.findByIdAndDelete(id).exec();
   },
 };
